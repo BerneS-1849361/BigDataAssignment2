5b39c977655aa6d39f1d5067820ec7507832fb4e
Berne <sannen.berne@gmail.com>
2021-10-21 15:48:57 +0200

    Made parser

---
+++ main.py
@@ -0,0 +1,68 @@
+import itertools
+import xml.sax
+# Author: Jasper Volders
+# Author: Berne Sannen
+
+
+
+class Handler(xml.sax.ContentHandler):
+    def __init__(self, field):
+        self.title = ""
+        self.year = 0
+        self.key = ""
+        self.field = field
+        self.skip = False
+
+        
+
+
+    # Call when an element starts
+    def startElement(self, tag, attributes):
+        self.CurrentData = tag
+        # get the scientific field of the article and set it to skip if it is an other field
+        if tag == "article" or tag == "inproceedings" or tag == "proceedings" or tag == "book" or \
+                tag == "incollection" or tag == "phdthesis" or tag == "mastersthesis" or tag == "www":
+            self.skip = False
+            self.key = attributes["key"]
+            self.key = self.key.split("/")
+            if self.key[1] == self.field:
+                self.skip = True
+
+    # Call when an elements ends
+    def endElement(self, tag):
+        if self.skip:
+            self.CurrentData = ""
+            return
+        # if the element is an article add the articles combinations to the bucket
+        if tag == "article" or tag == "inproceedings" or tag == "proceedings" or tag == "book" or \
+                tag == "incollection" or tag == "phdthesis" or tag == "mastersthesis" or tag == "www":
+            print(str(self.year) + ": " + self.title)
+        self.CurrentData = ""
+
+    # Call when a character is read
+    def characters(self, content):
+        if self.skip:
+            return
+        if self.CurrentData == "year":
+            self.year = int(content)
+        if self.CurrentData == "title":
+            self.title = content
+
+if __name__ == '__main__':
+    source = "dblp50000.xml"
+
+    # create an XMLReader
+    parser = xml.sax.make_parser()
+    # turn off namepsaces
+    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+
+    # override the default ContextHandler
+    handler = Handler("pkdd")
+    parser.setContentHandler(handler)
+
+    #parse file with the first pass
+    parser.parse(source)
+
+
+
+
