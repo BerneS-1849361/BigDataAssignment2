eb85031ea8ac4ec2186011b57600ef2a4cd5a317
Berne <sannen.berne@gmail.com>
2021-10-21 16:58:43 +0200

    bdscan

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import xml.sax
-from sklearn.cluster import MiniBatchKMeans
+from leven import levenshtein
+import numpy as np
+from sklearn.cluster import dbscan
+import matplotlib.pyplot as plt
 # Author: Jasper Volders
 # Author: Berne Sannen
-
 
 
 class Handler(xml.sax.ContentHandler):
@@ -13,8 +15,7 @@ class Handler(xml.sax.ContentHandler):
         self.field = field
         self.skip = False
 
-
-
+        self.data = []
 
     # Call when an element starts
     def startElement(self, tag, attributes):
@@ -25,7 +26,7 @@ class Handler(xml.sax.ContentHandler):
             self.skip = False
             self.key = attributes["key"]
             self.key = self.key.split("/")
-            if self.key[1] == self.field:
+            if self.key[1] != self.field:
                 self.skip = True
 
     # Call when an elements ends
@@ -36,7 +37,8 @@ class Handler(xml.sax.ContentHandler):
         # if the element is an article add the articles combinations to the bucket
         if tag == "article" or tag == "inproceedings" or tag == "proceedings" or tag == "book" or \
                 tag == "incollection" or tag == "phdthesis" or tag == "mastersthesis" or tag == "www":
-            print(str(self.year) + ": " + self.title)
+            if self.year > 1990 and self.year <= 2000:
+                self.data.append(self.title)
         self.CurrentData = ""
 
     # Call when a character is read
@@ -48,6 +50,10 @@ class Handler(xml.sax.ContentHandler):
         if self.CurrentData == "title":
             self.title = content
 
+def lev_metric(x, y):
+    i, j = int(x[0]), int(y[0])     # extract indices
+    return levenshtein(handler.data[i], handler.data[j])
+
 if __name__ == '__main__':
     source = "dblp50000.xml"
 
@@ -57,11 +63,25 @@ if __name__ == '__main__':
     parser.setFeature(xml.sax.handler.feature_namespaces, 0)
 
     # override the default ContextHandler
+
     handler = Handler("pkdd")
     parser.setContentHandler(handler)
 
+
     #parse file with the first pass
     parser.parse(source)
+
+
+    print(handler.data)
+    print(len(handler.data))
+
+    X = np.arange(len(handler.data)).reshape(-1, 1)
+    dbscan(X, metric=lev_metric, eps=5, min_samples=2)
+
+
+
+
+
 
 
 
